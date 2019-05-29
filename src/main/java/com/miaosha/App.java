126f4c95f06e80441319db442a52d89d45905e2f
zhang <651831787@qq.com>
2019-05-29 22:37:04 +0800

    集成redis半成品

--- src/main/java/com/miaosha/App.java
+++ src/main/java/com/miaosha/App.java
@@ -1,13 +1,21 @@
 package com.miaosha;
 
+import javafx.application.Application;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
+@MapperScan(basePackages = "com.miaosha.dao")
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        SpringApplication.run(App.class, args);
     }
 }
